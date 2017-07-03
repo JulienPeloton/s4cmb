77bb0edc623edd0ccc4956e89cbf1eb946adbb9b
peloton <j.peloton@sussex.ac.uk>
2017-07-03 09:39:26 +0100

    import of the script to generate hardware

---
+++ instrument/hardware.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+"""
+Script to simulate the focal plane of a CMB experiment.
+The hardware configuration is stored in a xml file, and read later on.
+Author: Julien Peloton, j.peloton@sussex.ac.uk
+"""
