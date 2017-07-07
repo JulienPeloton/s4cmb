affe3f8c2e5e6e1b9c80727b4de69b55a61f49dd
peloton <j.peloton@sussex.ac.uk>
2017-07-07 14:02:26 +0100

    Wrapper around coverage.py

---
+++ coverage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## Script to measure the coverage of the test suite (via doctest).
+## Launch it using ./coverage
+## and open the html files under the folder htmlcov/
+for i in instrument/*.py
+do  
+ echo 'coverage for '$i 
+ coverage run $i
+ coverage report $i 
+ coverage html $i
+done
+
