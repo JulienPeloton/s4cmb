8eb15ad7d961c4397d84822d620d518d78daca1f
peloton <j.peloton@sussex.ac.uk>
2017-07-17 17:51:47 +0100

    typo...

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from numpy.distutils.core import setup
 from numpy.distutils.misc_util import Configuration
 
 def configuration(parent_package='', top_path=None):
-    config = Configuration(name='s4cmb', parent_package, top_path)
+    config = Configuration('s4cmb', parent_package, top_path)
     config.add_extension('scanning_strategy_f',
                          sources=['s4cmb/scanning_strategy_f.f90'],
                          libraries=[], f2py_options=[],
